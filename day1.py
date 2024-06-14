d5a3d2d5e388e9e09d4f9729f5219b149dac7fbc
Your Name <you@example.com>
2024-06-14 18:31:37 +0530

    even and add.

--- day1.py
+++ day1.py
@@ -1,4 +1,16 @@
 name=['a','b','c','d']
 for item in name:
     print(item)
+    
+    number= [1,2,3,4,5,6.7,8,9, 10]
+for item in number:
+
+  if item % 2 == 0:
+    print ( "even",item)
+  else:
+      print("add",item)
+    
+
+
+
       
